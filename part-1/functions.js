68d4a374eae17e62d9575d2320b166e7fd2dc49a
Roel Cabrera <hellaboredguy@Roels-MacBook-Pro.local>
2017-06-19 23:18:25 -0700

    two functions made no tests yet

---
+++ part-1/functions.js
@@ -0,0 +1,26 @@
+//show a weekday
+
+//get a snippet from text
+
+function snippet(string, maxlength){
+  if (string.length > maxlength){
+    console.log(string.slice(0, maxlength) + "...");
+  }else {
+		console.log(string);
+	}
+};
+
+//number of properties
+
+
+//filter between
+
+function filterBetween(arr, min, max){
+  var newArr = [];
+  for(var i=0; i<=arr.length; i++){
+    if(arr[i]>min && arr[i]<max){
+    newArr.push(arr[i]);
+    };
+  };
+  console.log(newArr);
+};
